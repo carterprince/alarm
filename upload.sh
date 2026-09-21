86f8905897761ff4d3465323ffd7e74ba6c8f518
Carter Prince <carteraprince@gmail.com>
2026-09-21 14:28:58 -0400

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+sudo chmod 666 /dev/ttyACM0
 arduino-cli compile --fqbn arduino:renesas_uno:unor4wifi -p /dev/ttyACM0 -u . && arduino-cli monitor -p /dev/ttyACM0 --config baudrate=115200
